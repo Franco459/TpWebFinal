3e68856aee960f94e12a82f39ae662e2d89af828
MathCh <chavezmatias999@gmail.com>
2018-07-05 15:14:56 -0300

    Modelo Novedades

--- Front-end/src/app/componetns/novedades/novedades.component.ts
+++ Front-end/src/app/componetns/novedades/novedades.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-novedades',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./novedades.component.css']
 })
 export class NovedadesComponent implements OnInit {
+  nump = 0; numd = 0; texto = "";
+  tamMaximo = 150; longMensaje = 0;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
   }
 
+  cambLong(){
+    this.longMensaje = this.texto.length;
+  }
+
+  enviarMsj(modal){
+    this.modalService.open(modal, {centered: true});
+  }
+
+  limpiar(){
+    this.nump = 0;
+    this.numd = 0;
+    this.texto = "";
+  }
 }
